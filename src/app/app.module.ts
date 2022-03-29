951643a1b6df4ddb53583a48a4571414dbfeee17
shereen <gmshereen98@gmail.com>
2022-03-29 11:09:39 +0200

    install bootstrap and creat components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatListComponent } from './components/chat-list/chat-list.component';
+import { ChatBodyComponent } from './components/chat-body/chat-body.component';
+import { ChatBoxComponent } from './components/chat-box/chat-box.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatListComponent,
+    ChatBodyComponent,
+    ChatBoxComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
